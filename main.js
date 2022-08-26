56a42d80da43fbc1190e96db4e06ecab95685e12
MiniKraken <9305105+MiniKraken-Team@users.noreply.github.com>
2022-08-26 10:51:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+module.exports.templateTags = [{
+    name: 'adonis_csrf',
+    displayName: 'Adonis CSRF',
+    description: 'Apply XSRF-TOKEN from cookie for X-XSRF-TOKEN request header',
+    args: [
+        {
+            displayName: 'CSRF Cookie Name',
+            type: 'string',
+            defaultValue: 'XSRF-TOKEN',
+            placeholder: 'It is "XSRF-TOKEN" by default in adonis',
+        }
+    ],
+    async run(context, cookieName = 'XSRF-TOKEN') {
+        const cookieJar = await this.getCookieJar(context);
+
+        const token = cookieJar.cookies?.find(cookie => cookie.key === cookieName);
+
+        if (token === undefined) {
+            throw new Error(`${cookieName} not found in cookies`);
+        }
+
+        return token.value;
+    },
+
+    async getCookieJar(context) {
+        const { meta } = context;
+
+        if (!meta.requestId || !meta.workspaceId) {
+            throw new Error(`Request ID or workspace ID not found`);
+        }
+
+        const workspace = await context.util.models.workspace.getById(meta.workspaceId);
+
+        if (!workspace) {
+            throw new Error(`Workspace not found for ${meta.workspaceId}`);
+        }
+
+        const cookieJar = await context.util.models.cookieJar.getOrCreateForWorkspace(workspace);
+
+        if (!cookieJar) {
+            throw new Error(`Cookie jar not found for ${meta.workspaceId}`);
+        }
+
+        return cookieJar;
+    },
+}];
